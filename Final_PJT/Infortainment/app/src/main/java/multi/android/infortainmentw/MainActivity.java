177bdfaa8f2f1e21bf61645d526b637cd1bab493
yuhwanwoo <6514687@naver.com>
2020-05-14 16:55:10 +0900

    music frag init

--- Final_PJT/Infortainment/app/src/main/java/multi/android/infortainmentw/MainActivity.java
+++ Final_PJT/Infortainment/app/src/main/java/multi/android/infortainmentw/MainActivity.java
@@ -1,23 +1,36 @@
 package multi.android.infortainmentw;
 
+import android.annotation.SuppressLint;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import multi.android.infortainmentw.control.Control;
+import multi.android.infortainmentw.music.MusicFragment;
 
 public class MainActivity extends AppCompatActivity {
     Control control = new Control();
+    MusicFragment musicFragment=new MusicFragment();
+    @SuppressLint("SourceLockedOrientationActivity")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+
         FragmentManager fragmentManager;
         fragmentManager = getSupportFragmentManager();
         FragmentTransaction transaction;
         transaction = fragmentManager.beginTransaction();
         transaction.replace(R.id.fragment_control,control);
+        transaction.replace(R.id.fragment_music,musicFragment);
+
     }
 }
