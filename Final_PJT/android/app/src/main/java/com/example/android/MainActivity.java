b6c3ef78a1a473a8abbf009d15c17b9e2dfe336a
insunkang <33975738+insunkang@users.noreply.github.com>
2020-05-25 13:40:20 +0900

    Revert "update app_main & spring -ksh"

--- Final_PJT/android/app/src/main/java/com/example/android/MainActivity.java
+++ Final_PJT/android/app/src/main/java/com/example/android/MainActivity.java
@@ -1,34 +1,14 @@
 package com.example.android;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageButton;
-
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.example.android.car.Car;
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-    ImageButton main_home_img;
-    ImageButton main_car_img;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        main_home_img = findViewById(R.id.main_home_img);
-        main_car_img = findViewById(R.id.main_car_img);
-
-        main_home_img.setImageResource(R.mipmap.main_home_img);
-        main_car_img.setImageResource(R.mipmap.main_car_img);
-
-        main_car_img.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Car.class);
-                startActivity(intent);
-            }
-        });
     }
 }
