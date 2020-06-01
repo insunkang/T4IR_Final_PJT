a53e5d1b1b6abda00bdeb44949244ea65c2a0ee4
Baek-SeungJae <tmdwo897@naver.com>
2020-06-01 15:10:50 +0900

    6/1 인포테인먼트 병합 전 백업

--- Final_PJT/Infortainment/app/src/main/java/multi/android/infortainmentw/Variable.java
+++ Final_PJT/Infortainment/app/src/main/java/multi/android/infortainmentw/Variable.java
@@ -1,6 +1,6 @@
 package multi.android.infortainmentw;
 
 public class Variable {
-    public static String springIP = "192.168.43.253";
-    public static String tcpIP = "192.168.43.253";
+    public static String springIP = "70.12.224.117";
+    public static String tcpIP = "70.12.224.117";
 }
