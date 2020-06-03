deebccd0bc5fa560aba34c54350334dfb96d07e3
Baek-SeungJae <tmdwo897@naver.com>
2020-06-03 10:59:00 +0900

    6/3 내차찾기버튼 수정

--- Final_PJT/Infortainment/app/src/main/java/multi/android/infortainmentw/Variable.java
+++ Final_PJT/Infortainment/app/src/main/java/multi/android/infortainmentw/Variable.java
@@ -2,5 +2,5 @@ package multi.android.infortainmentw;
 
 public class Variable {
     public static String springIP = "70.12.224.117";
-    public static String tcpIP = "70.12.224.117";
+    public static String tcpIP = "70.12.116.58";
 }
